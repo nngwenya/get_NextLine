7496f52a66f6aa4f377a8bf58561cb4b009f245e
Franco SCHEEPERS <fscheepe@e4r19p1.jnb.42.fr>
2017-06-19 12:39:30 +0200

    0

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nngwenya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/16 23:54:17 by nngwenya          #+#    #+#             */
-/*   Updated: 2017/06/17 00:32:08 by nngwenya         ###   ########.fr       */
+/*   Updated: 2017/06/19 12:35:36 by fscheepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,38 @@
 #include "stdio.h"
 
 
-int		ft_get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	int p_fd;
-	char buffer[BUFF_SIZE + 1];
+	int read_bytes;
+	static char buffer[BUFF_SIZE + 1];
 	char *str;
-	int i;
 
-	str = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
-	p_fd = read(fd, buffer, BUFF_SIZE + 1);
+	read_bytes = read(fd, buffer, BUFF_SIZE);
 	buffer[BUFF_SIZE] = '\0';
-	i = 0;
-	while (i < BUFF_SIZE + 1)
-	{
-		str[i] = buffer[i];
-		i++;
-	}
+	str = ft_strjoin(*line, buffer);
+	free(*line);
 	*line = str;
 
-	return (1);
+	return (read_bytes);
+}
+
+int		main(void)
+{
+	int		fd = open("wethinkcode.txt", O_RDONLY);
+	char	*str;
+	int		status = 0;
+	int		i = 0;
+
+	str = (char *)malloc(sizeof(char));
+	while (i < 100)
+	{
+		status = get_next_line(fd, &str);
+
+		if (status == 1)
+			printf("%s\n", str);
+		else
+			printf("Error");
+		i++;
+	}
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,22 @@
 /*   By: nngwenya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 00:07:47 by nngwenya          #+#    #+#             */
-/*   Updated: 2017/06/17 00:08:39 by nngwenya         ###   ########.fr       */
+/*   Updated: 2017/06/19 12:35:56 by fscheepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __GET_NEXT_LINE_H
 # define __GET_NEXT_LINE_H
+# define BUFF_SIZE 64
 
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "libft.h"
 
-# define BUFF_SIZE 100000
+int		get_next_line(const int fd, char **line);
 
-int		ft_get_next_line(const int fd, char **line);
 #endif
